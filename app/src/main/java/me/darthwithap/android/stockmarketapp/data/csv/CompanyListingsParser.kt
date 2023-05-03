8b3366d29ec39b0a50c7b1e640813dbff00271b5
Parth Takkar <parthtakkar@gmail.com>
2023-05-03 23:47:48 +0530

    CompanyListingsParser.kt implemented

---
+++ app/src/main/java/me/darthwithap/android/stockmarketapp/data/csv/CompanyListingsParser.kt
@@ -0,0 +1,33 @@
+package me.darthwithap.android.stockmarketapp.data.csv
+
+import com.opencsv.CSVReader
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import me.darthwithap.android.stockmarketapp.domain.model.CompanyListing
+import java.io.InputStream
+import java.io.InputStreamReader
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CompanyListingsParser @Inject constructor() : CsvParser<CompanyListing> {
+
+  override suspend fun parse(stream: InputStream): List<CompanyListing> {
+    val csvReader = CSVReader(InputStreamReader(stream))
+
+    return withContext(Dispatchers.IO) {
+
+      csvReader.readAll().drop(1).mapNotNull { element ->
+        val symbol = element.getOrNull(0)
+        val name = element.getOrNull(1)
+        val exchange = element.getOrNull(2)
+
+        CompanyListing(
+          name = name ?: return@mapNotNull null,
+          symbol = symbol ?: return@mapNotNull null,
+          exchange = exchange ?: return@mapNotNull null
+        )
+      }
+    }.also { csvReader.close() }
+  }
+}
---
+++ app/src/main/java/me/darthwithap/android/stockmarketapp/data/csv/CsvParser.kt
@@ -0,0 +1,7 @@
+package me.darthwithap.android.stockmarketapp.data.csv
+
+import java.io.InputStream
+
+interface CsvParser<T> {
+  suspend fun parse(stream: InputStream): List<T>
+}
